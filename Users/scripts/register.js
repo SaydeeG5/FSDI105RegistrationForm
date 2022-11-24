159384b22273e182e087bb71a29a1eb3904f6a88
Maria "Saydee" Guevarra <114355214+SaydeeG5@users.noreply.github.com>
2022-11-23 22:19:08 -0800

    Add files via upload

--- Users/scripts/register.js
+++ Users/scripts/register.js
@@ -1,58 +1,106 @@
-//create the constructor 
-function User(fname,lname,age,gender,phone,payment,color){
-    //list of attributes
-    this.firstName = fname;
-    this.lastName = lname;
-    this.age = age;
-    this.gender = gender;
-    this.phone = phone;
-    this.payment = payment;
-    this.color = color;
+//create the constructor ; declares with capital letter
+function User(fname, lname, age, gender, phone, payment, color) {
+  //list of attributes
+  this.firstName = fname;
+  this.lastName = lname;
+  this.age = age;
+  this.gender = gender;
+  this.phone = phone;
+  this.payment = payment;
+  this.color = color;
 }
 
-function register(){
-    let inputFirstName = $("#txtFirstName").val();
-    let inputLastName = $("#txtLastName").val();
-    let inputAge = $("#txtAge").val();
-    let inputGender = $("#txtGender").val();
-    let inputPhone = $("#txtPhone").val();
-    let inputPayment = $("#selPayment").val();
-    let inputColor = $("#selColor").val();
+function register() {
+  let inputFirstName = $("#txtFirstName").val();
+  let inputLastName = $("#txtLastName").val();
+  let inputAge = $("#txtAge").val();
+  let inputGender = $("#txtGender").val();
+  let inputPhone = $("#txtPhone").val();
+  let inputPayment = $("#selPayment").val();
+  let inputColor = $("#selColor").val();
 
-    //validations
-    if(!inputFirstName || !inputLastName) {
-        console.error("Invalid data");
-        return; // get out of the fn, do not continue  
-    }
+  //validations
 
-    let newUser = new User(inputFirstName, inputLastName, inputAge, inputGender, inputPhone, inputPayment,inputColor)
-    console.log(newUser);
-    
-    saveUser(newUser); 
-    console.log("User Saved");
+  if (!inputFirstName || !inputLastName) {
+    console.error("Invalid data");
+    //alert("Error: First and Last Name required");
+    $(".error-msg").show();
+    setTimeout(function () {
+      $(".error-msg").hide();
+    }, 5000);
 
-    clearForm();
+    return; // get out of the fn, do not continue
+  }
+
+  let newUser = new User(
+    inputFirstName,
+    inputLastName,
+    inputAge,
+    inputGender,
+    inputPhone,
+    inputPayment,
+    inputColor
+  );
+  console.log(newUser);
+
+  saveUser(newUser);
+  displayUser(newUser);
+  $("#pnlSuccess").show();
+
+  //hide the panel after 6 seconds
+  setTimeout(function () {
+    $("#pnlSuccess").hide();
+  }, 6000);
+
+  console.log("User Saved!");
+
+  clearForm();
 }
 
 function clearForm() {
-    $("#txtFirstName").val("");
-    $("#txtLastName").val("");
-    $("#txtAge").val("");
-    $("#txtGender").val("");
-    $("#txtPhone").val("");
-    $("#selColor").val("000000");
+  $("#txtFirstName").val("");
+  $("#txtLastName").val("");
+  $("#txtAge").val("");
+  $("#txtGender").val("");
+  $("#txtPhone").val("");
+  $("#selPayment").val("");
+  $("#selColor").val("000000");
+}
+
+function displayUser(user) {
+  let syntax = `
+            <tr class="inputInfo">
+                <td>${user.firstName}</td>
+                <td>${user.lastName}</td>
+                <td>${user.age}</td>
+                <td>${user.gender}</td>
+                <td>${user.payment}</td>
+                <td>${user.color}</td>
+                
+            </tr>`;
+  $("#table-user > tbody").append(syntax);
+}
+
+function loadUser() {
+  //get data from local storage
+  let all = getAllUsers(); // [x,x,x,x,x,x,]
+  //console.log(all);
+  for (let i = 0; i < all.length; i++) {
+    let user = all[i];
+    displayUser(user);
+  }
 }
 
 function init() {
-    //html is ready
-    console.log("Page ready!");
+  //html is ready
+  console.log("Page ready!");
 
-    //load data 
+  //load data
+  loadUser();
 
-    //assigns events 
-    $("#btnSave").click(register);
+  //assigns events
+  $("#btnSave").click(register);
 }
 
 // wait for html to render before javascript is loaded
 window.onload = init;
-
--- Users/scripts/storage.js
+++ Users/scripts/storage.js
@@ -1,26 +1,23 @@
-
-
 function saveUser(user) {
-    let existing = getAllUsers(); //read 
-    existing.push(user); // add new user to the list
+  let existing = getAllUsers(); //read
+  existing.push(user); // add new user to the list
 
-    // JSON = JavaScript Object Notation 
-    let value = JSON.stringify(existing); // <-- parse the object into a json string 
+  // JSON = JavaScript Object Notation
+  let value = JSON.stringify(existing); // <-- parse the object into a json string
 
-    // localStorage only stores: string, number so we will encode our object to string  
-    localStorage.setItem("users", value)
+  // localStorage only stores: string, number so we will encode our object to string
+  localStorage.setItem("users", value);
 }
 
 function getAllUsers() {
-    let allUsers = [];
+  let allUsers = [];
 
-    let string = localStorage.getItem("users"); //read the jason string 
-    if(string) {
-        //parse the string into an array 
-        allUsers = JSON.parse(string); //parse is the opposite of JSON.stringify
-        return allUsers;
-    }
-    else {
-        return [];
-    }
+  let string = localStorage.getItem("users"); //read the jason string
+  if (string) {
+    //parse the string into an array
+    allUsers = JSON.parse(string); //parse is the opposite of JSON.stringify
+    return allUsers;
+  } else {
+    return [];
+  }
 }
